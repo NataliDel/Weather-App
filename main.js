6844055655ef3bbc51ed47e76d605b666f2c668c
Testname <nataliadel7@gmail.com>
2025-09-19 16:17:33 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+import { loadMainMenu } from "./src/mainMenu";
+import "./styles/styles.scss";
+
+export const rootElement = document.getElementById("app");
+
+//loadDetailView("Leverkusen");
+loadMainMenu();
---
+++ src/api.js
@@ -0,0 +1,49 @@
+const API_BASE_URL = "http://api.weatherapi.com/v1";
+const API_KEY = "4d3dc4d28cd947078a9141327251705";
+const FAVORITE_CITIES_KEY = "favorite-cities";
+
+export async function getForecastWeather(location, days = 3) {
+  const response = await fetch(
+    `${API_BASE_URL}/forecast.json?key=${API_KEY}&q=id:${location}&lang=de&days=${days}`
+  );
+
+  const weatherData = await response.json();
+
+  console.log(weatherData);
+
+  return weatherData;
+}
+
+export async function searchLocation(q) {
+  const response = await fetch(
+    `${API_BASE_URL}/search.json?key=${API_KEY}&q=${q}&lang=de`
+  );
+
+  const searchResults = await response.json();
+
+  return searchResults;
+}
+
+export function getFavoriteCities() {
+  return JSON.parse(localStorage.getItem(FAVORITE_CITIES_KEY)) || [];
+}
+
+export function saveCityAsFavorite(city) {
+  const favorites = getFavoriteCities();
+
+  if (favorites.find((favorite) => favorite === city)) {
+    alert(city + " wurde bereits den Favoriten hinzugefügt!");
+    return;
+  }
+  favorites.push(city);
+
+  localStorage.setItem(FAVORITE_CITIES_KEY, JSON.stringify(favorites));
+}
+
+export function removeCityFromFavorites(city) {
+  const favorites = getFavoriteCities();
+
+  const filteredFavorites = favorites.filter((favorite) => favorite !== city);
+
+  localStorage.setItem(FAVORITE_CITIES_KEY, JSON.stringify(filteredFavorites));
+}
